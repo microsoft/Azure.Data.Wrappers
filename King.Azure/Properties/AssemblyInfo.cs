4de6880e31bcec7e1636eb4df539d1b8f9748f4f
Jef King <jef@agilebusinesscloud.com>
2014-08-16 22:25:20 -0700

    Basic Solution

---
+++ King.Azure/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("King.Azure")]
+[assembly: AssemblyDescription("Azure Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("King.Azure")]
+[assembly: AssemblyCopyright("Copyright © Jef King 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("4e7ae8fe-0ee3-44a4-bae7-2ea46062c10e")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
