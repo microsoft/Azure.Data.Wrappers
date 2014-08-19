d447a829401e264729975cbfc4fb739bbf59ddb4
Jef King <jef@agilebusinesscloud.com>
2014-08-18 20:04:03 -0700

    Unit Tests

--- King.Azure.Unit.Test/Data/ContainerTests.cs
+++ King.Azure.Unit.Test/Data/ContainerTests.cs
@@ -57,5 +57,48 @@
             var c = new Container("test", "UseDevelopmentStorage=true");
             await c.Save(null, new object());
         }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task SaveObjectNull()
+        {
+            var c = new Container("test", "UseDevelopmentStorage=true");
+            await c.Save(Guid.NewGuid().ToString(), null);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public async Task GetBytesBlobNameNull()
+        {
+            var c = new Container("test", "UseDevelopmentStorage=true");
+            await c.Get(null);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public async Task SaveBytesBlobNameNull()
+        {
+            var random = new Random();
+            var bytes = new byte[1024];
+            random.NextBytes(bytes);
+
+            var c = new Container("test", "UseDevelopmentStorage=true");
+            await c.Save(null, bytes);
+        }
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public async Task SaveBytesNull()
+        {
+            var c = new Container("test", "UseDevelopmentStorage=true");
+            await c.Save(Guid.NewGuid().ToString(), null);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public async Task GetReferenceBlobNameNull()
+        {
+            var c = new Container("test", "UseDevelopmentStorage=true");
+            await c.GetReference(null);
+        }
     }
 }
