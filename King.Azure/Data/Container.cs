4c8bb3f924063d13c0f1bd063fdd910edb7c7028
Jef King <jef@agilebusinesscloud.com>
2014-11-12 20:03:34 -0800

    sort

--- King.Azure/Data/Container.cs
+++ King.Azure/Data/Container.cs
@@ -4,9 +4,9 @@
     using Microsoft.WindowsAzure.Storage.Blob;
     using Newtonsoft.Json;
     using System;
-    using System.Linq;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using System.Threading.Tasks;
 
     /// <summary>
