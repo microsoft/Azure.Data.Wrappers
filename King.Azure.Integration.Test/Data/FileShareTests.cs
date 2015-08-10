7bdeca83e624129d249128ba60868b161e840c6e
Jef King <jef@agilebusinesscloud.com>
2015-08-10 14:34:34 -0700

    no guid

--- King.Azure.Integration.Test/Data/FileShareTests.cs
+++ King.Azure.Integration.Test/Data/FileShareTests.cs
@@ -13,8 +13,8 @@
         [Test]
         public async Task CreateIfNotExists()
         {
-            var name = 'a' + Guid.NewGuid().ToString().ToLowerInvariant().Replace('-', 'a');
-            var storage = new FileShare(name, ConnectionString);
+            var random = new Random();
+            var storage = new FileShare(string.Format("a{0}b", random.Next()), ConnectionString);
             var created = await storage.CreateIfNotExists();
 
             Assert.IsTrue(created);
