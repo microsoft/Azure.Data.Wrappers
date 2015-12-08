0db74f9c801afd7aeca9fbec3fe26d40709650f3
Jef King <jefkin@microsoft.com>
2015-12-08 09:50:24 -0800

    versions

--- King.Azure.Unit.Test/Properties/AssemblyInfo.cs
+++ King.Azure.Unit.Test/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("e925ec1b-23f6-4f01-9d9b-e076f9ffb84b")]
-[assembly: AssemblyVersion("1.0.0.18")]
-[assembly: AssemblyFileVersion("1.0.0.18")]
+[assembly: AssemblyVersion("1.0.0.19")]
+[assembly: AssemblyFileVersion("1.0.0.19")]
