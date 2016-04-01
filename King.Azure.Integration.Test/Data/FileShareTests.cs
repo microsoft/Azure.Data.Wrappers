7c2ebb05066cffde8fcb5067149993a84c185cd8
Jef King <jefkin@microsoft.com>
2016-04-01 15:35:38 -0700

    rfeady

--- King.Azure.Integration.Test/Data/FileShareTests.cs
+++ King.Azure.Integration.Test/Data/FileShareTests.cs
@@ -1,14 +1,13 @@
 ﻿namespace King.Azure.Integration.Test.Data
 {
-    using System;
-    using System.Threading.Tasks;
     using King.Azure.Data;
     using NUnit.Framework;
+    using System;
+    using System.Threading.Tasks;
 
     [TestFixture]
     public class FileShareTests
     {
-        private const string ConnectionString = "DefaultEndpointsProtocol=https;AccountName=kingazure;AccountKey=nCJim8J7DwyAsk1ULffZLCjKZ/iVvULDG3dlbTi8XoXUcztB1jQss3VbEHdVIUlkl3866JA0hg/01BTkFKklWQ==";
 
         [Test]
         public async Task CreateIfNotExists()
