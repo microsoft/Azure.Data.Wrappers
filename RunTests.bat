6550bc1f990e416e74d8f89ec10b46fd77eb20d5
Jef King <jefkin@microsoft.com>
2016-05-08 17:22:02 -0700

    test the codez

--- RunTests.bat
+++ RunTests.bat
@@ -5,3 +5,11 @@ dnu restore -f ../../../King.Azure/Release
 ./King.Azure.Unit.Test.cmd
 
 CD ../../../../../
+
+CD .\artifacts\bin\King.Azure.Integration.Test\Release\app
+
+dnvm use 1.0.0-rc1-update1 -r clr -a x64
+dnu restore -f ../../../King.Azure/Release
+./King.Azure.Integration.Test.cmd
+
+CD ../../../../../
