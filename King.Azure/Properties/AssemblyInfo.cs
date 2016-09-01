237d3d392984fe9f5b326f04336d57bd165424d4
Jef King <jefkin@microsoft.com>
2016-08-31 21:19:43 -0700

    more frameworks

--- King.Azure/Properties/AssemblyInfo.cs
+++ King.Azure/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("King.Azure")]
-[assembly: AssemblyDescription("King Azure Storage Library")]
+[assembly: AssemblyDescription("Azure Storage Simplified")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("King.Azure")]
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4e7ae8fe-0ee3-44a4-bae7-2ea46062c10e")]
-[assembly: AssemblyVersion("2.0.0.6")]
-[assembly: AssemblyFileVersion("2.0.0.6")]
+[assembly: AssemblyVersion("2.0.0.7")]
+[assembly: AssemblyFileVersion("2.0.0.7")]
