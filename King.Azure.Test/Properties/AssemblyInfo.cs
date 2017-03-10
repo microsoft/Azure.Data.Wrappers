5e48dcfafaf211aa3993ec1f4fb114e687777e8e
Jef King <jefkin@microsoft.com>
2017-03-10 11:04:45 -0800

    version

--- King.Azure.Test/Properties/AssemblyInfo.cs
+++ King.Azure.Test/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("King.Azure.Unit.Test")]
-[assembly: AssemblyCopyright("Copyright © Jef King 2016")]
+[assembly: AssemblyCopyright("Copyright © Jef King 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
